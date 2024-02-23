bc23ad22fef9178f7951a32d533e749e5c7b5f91
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-23 04:46:09 +0300

    Update App.js

--- Sellaro/App.js
+++ Sellaro/App.js
@@ -1,25 +1,19 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
-export default function App() {
+const App = () => {
   return (
-    <View
-      style={styles.container}
-      className="flex-1 items-center justify-center bg-[#ffd700]"
-    >
-      <Text className="text-[#32cd32] text-lg">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* <WelcomeScreen /> */}
+      <ViewImageScreen/>
+      
+      
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffd700",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
