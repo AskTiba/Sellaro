25229d99d914e0e0b0b545dddfbea408a55b0452
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-23 04:46:18 +0300

    Create ViewImageScreen.js

---
+++ Sellaro/app/screens/ViewImageScreen.js
@@ -0,0 +1,48 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Colors from "../config/Colors";
+
+const ViewImageScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/cup.png")}
+      />
+      <StatusBar style="light" />
+    </View>
+  );
+};
+
+export default ViewImageScreen;
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: Colors.primary,
+    position: "absolute",
+    top: 50,
+    right: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: Colors.secondary,
+    position: "absolute",
+    top: 50,
+    left: 30,
+  },
+  container: {
+    // flex: 1,
+    backgroundColor: "#000",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
