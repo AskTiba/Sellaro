f956cedb4e4d6a1cfbae3c8d10f604db37d77304
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-24 11:51:14 +0300

    Update tailwind.config.js

--- Sellaro/tailwind.config.js
+++ Sellaro/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./App.{js,jsx,ts,tsx}",
-  "./app/**/*.{js,jsx,ts,tsx}",
+  "./screens/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
   extend: {},
