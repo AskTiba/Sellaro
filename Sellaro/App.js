cba51a45c881c83a3244f695624b955dc23bb393
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-24 11:51:18 +0300

    Update App.js

--- Sellaro/App.js
+++ Sellaro/App.js
@@ -2,15 +2,17 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Navigation from "./Navigation";
+import "react-native-gesture-handler";
 
 const App = () => {
   return (
-    <View>
-      <WelcomeScreen />
-      {/* <ViewImageScreen/> */}
-      
-      
-    </View>
+    // <View>
+    //   {/* <WelcomeScreen /> */}
+    //   {/* <ViewImageScreen/> */}
+    //   <Navigation />
+    // </View>
+    <Navigation />
   );
 };
 
