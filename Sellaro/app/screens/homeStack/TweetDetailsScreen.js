db53d4e2d39fd381ba9979666d8f70654743766f
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-26 05:26:19 +0300

    Create TweetDetailsScreen.js

---
+++ Sellaro/app/screens/homeStack/TweetDetailsScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const TweetDetailsScreen = () => {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>TweetDetailsScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default TweetDetailsScreen;
+
+const styles = StyleSheet.create({});
